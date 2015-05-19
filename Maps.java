23a145dfa1579927451a6f2fe446b78ea4a949fa
Jay Patel <jpatel5@students.d125.org>
2015-05-19 09:47:37 -0500

    ugh

---
+++ Maps.java
@@ -0,0 +1,121 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+/**
+ * Write a description of class Map here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Maps
+{
+
+private ArrayList <String> countries;
+private ArrayList <String> capitals;
+
+public Maps()
+{
+    countries.add("Afghanistan");
+    countries.add("Armenia");
+    countries.add("Azerbaijan");
+    countries.add("Bahrain");
+    countries.add("Bangladesh");
+    countries.add("Bhutan");
+    countries.add("Brunei");
+    countries.add("Cambodia");
+    countries.add("China");
+    countries.add("Cyprus");
+    countries.add("Gerogia");
+    countries.add("Hindustan");
+    countries.add("Indonesia");
+    countries.add("Iran");
+    countries.add("Iraq");
+    countries.add("Israel");
+    countries.add("Japan");
+    countries.add("Jordan");
+    countries.add("Kazakhstan");
+    countries.add("Kuwait");
+    countries.add("Kyrgyzstan");
+    countries.add("Laos");
+    countries.add("Lebanon");
+    countries.add("Malaysia");
+    countries.add("Maldives");
+    countries.add("Mongolia");
+    countries.add("Mynammar");
+    countries.add("Nepal");
+    countries.add("North Korea");
+    countries.add("Oman");
+    countries.add("Pakistan");
+    countries.add("Palestine");
+    countries.add("Phillipines");
+    countries.add("Qatar");
+    countries.add("Saudi Arabia");
+    countries.add("Singapore");
+    countries.add("South Korea");
+    countries.add("Sri Lanka");
+    countries.add("Syria");
+    countries.add("Taiwan");
+    countries.add("Tajikistan");
+    countries.add("Thailand");
+    countries.add("Timor-Leste");
+    countries.add("Turkey");
+    countries.add("Turkmenistan");
+    countries.add("United Arab Emirates");
+    countries.add("Uzbekistan");
+    countries.add("Vietnam");
+    countries.add("Yemen");
+    capitals.add("Kabul");
+    capitals.add("Yerevan");
+    capitals.add("Baku");
+    capitals.add("Manama");
+    capitals.add("Dhaka");
+    capitals.add("Thimpu");
+    capitals.add("Bandar Seri Bagawan");
+    capitals.add("Phnom Penh");
+    capitals.add("Beijing");
+    capitals.add("Nicosia");
+    capitals.add("Tbilisi");
+    capitals.add("New Delhi");
+    capitals.add("Jakarta");
+    capitals.add("Tehran");
+    capitals.add("Baghdad");
+    capitals.add("Jerusalem");
+    capitals.add("Tokoyo");
+    capitals.add("Amman");
+    capitals.add("Astana");
+    capitals.add("Kuwait City");
+    capitals.add("Bishkek");
+    capitals.add("Vientiane");
+    capitals.add("Beirut");
+    capitals.add("Kuala Lumpur");
+    capitals.add("Male");
+    capitals.add("Ulan Bator");
+    capitals.add("Naypyidaw");
+    capitals.add("Kathmandu");
+    capitals.add("Pyoungyang");
+    capitals.add("Muscat");
+    capitals.add("Islamabad");
+    capitals.add("Jerusalem");
+    capitals.add("Manila");
+    capitals.add("Doha");
+    capitals.add("Riyadh");
+    capitals.add("Singapore City");
+    capitals.add("Seoul");
+    capitals.add("Colombo");
+    capitals.add("Damascus");
+    capitals.add("Taipei");
+    capitals.add("Dusanbe");
+    capitals.add("Bangkok");
+    capitals.add("Dilli");
+    capitals.add("Ankara");
+    capitals.add("Ashgabat");
+    capitals.add("Abu Dhabi");
+    capitals.add("Tashkent");
+    capitals.add("Hanoi");
+    capitals.add("Sanna");
+    
+
+   
+
+}
+}
