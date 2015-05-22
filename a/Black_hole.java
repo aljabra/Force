9c3bea44544b1aa4596f2bf61451d122dbb4734a
aljabra <jpatel5@students.d125.org>
2015-05-21 21:42:18 -0500

    a
    a

---
+++ a/Black_hole.java
@@ -0,0 +1,22 @@
+package a;
+
+
+/**
+ * Write a description of class Black_hole here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Black_hole
+{
+    // instance variables - replace the example below with your own
+    final private int x;
+
+    /**
+     * Constructor for objects of class Black_hole
+     */
+    public Black_hole(double constant, double intialDistance, double productCharges)
+    {
+        
+    }
+}
