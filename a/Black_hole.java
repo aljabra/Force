3fa37e96fa03a578599db7e616b1631d462af6c9
aljabra <jpatel5@students.d125.org>
2015-05-21 21:55:52 -0500

    a

--- a/Black_hole.java
+++ a/Black_hole.java
@@ -10,13 +10,36 @@ package a;
 public class Black_hole
 {
     // instance variables - replace the example below with your own
-    final private int x;
-
+    private double x;
+    final private double q;
+    final private double k = 9;
+    final private double t;
+    private double a = 0, v = 0;
     /**
      * Constructor for objects of class Black_hole
      */
     public Black_hole(double constant, double intialDistance, double productCharges)
     {
-        
+        x = intialDistance;
+        q = productCharges;
+        t = constant;
+    }
+    public void change()
+    {
+        a = Math.pow(1/x,2);
+        v = a*t + v;
+        x = x + v*t + 1/2*a*t*t;
+    }
+    public double getAcceleration()
+    {
+        return a;
+    }
+    public double getVelocity()
+    {
+        return v;
+    }
+    public double getDisplacement()
+    {
+        return x;
     }
 }
